90b716778e29e87ccd3972aa8c210fc06529f605
denizturgut99 <denizturgut1999@hotmail.com>
2019-03-15 14:48:05 +0100

    added 5 day forecast

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ var app = new Vue({
     el: "#app",
     data: {
         weatherData: [],
+        weatherForecast: [],
         search: "",
+        view:""
     },
     methods: {
         callAllFuncs() {
+            var url2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + this.search + "&appid=c86be36a020f3d6fc311354ac9cc9878&units=metric"
             var url = "https://api.openweathermap.org/data/2.5/weather?q=" + this.search + "&appid=c86be36a020f3d6fc311354ac9cc9878&units=metric"
-            
+
             fetch(url, {
                     mode: "cors"
                 })
@@ -19,10 +22,22 @@ var app = new Vue({
                     app.weatherData = newJson
                 })
                 .catch(error => console.log(error));
+
+            fetch(url2, {
+                    mode: "cors"
+                })
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (forecastJson) {
+                    console.log(forecastJson);
+                    app.weatherForecast = forecastJson.list
+                })
+            .catch(error => console.log(error))
+
         },
         getCity() {
             this.callAllFuncs()
         }
     }
 })
-
