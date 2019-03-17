1d783f2075646d33085274bab73861726b447a60
denizturgut99 <denizturgut1999@hotmail.com>
2019-03-17 15:43:38 +0100

    small design update

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ var app = new Vue({
     data: {
         weatherData: [],
         weatherForecast: [],
+        forecastDates: [],
         search: "",
-        view:""
+        view: ""
     },
     methods: {
         callAllFuncs() {
             var url2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + this.search + "&appid=c86be36a020f3d6fc311354ac9cc9878&units=metric"
             var url = "https://api.openweathermap.org/data/2.5/weather?q=" + this.search + "&appid=c86be36a020f3d6fc311354ac9cc9878&units=metric"
+
+            var currentDate;
+            var forecastArr;
+            var currentArr;
+            var forDate;
 
             fetch(url, {
                     mode: "cors"
@@ -20,6 +26,10 @@ var app = new Vue({
                 .then(function (newJson) {
                     console.log(newJson)
                     app.weatherData = newJson
+                    currentArr = newJson
+                    currentDate = newJson.dt
+                    //                    setImage()
+                    //                    console.log(currentDate);
                 })
                 .catch(error => console.log(error));
 
@@ -31,13 +41,25 @@ var app = new Vue({
                 })
                 .then(function (forecastJson) {
                     console.log(forecastJson);
-                    app.weatherForecast = forecastJson.list
+                    app.weatherForecast = forecastJson.list;
+                    forecastArr = forecastJson.list;
+//                    findDates();
+                    //                    app.forecastDates = forecastJson.list.dt;
+                    //                    forDate = forecastJson.list.dt;
+                    //                    console.log(forDate);
                 })
-            .catch(error => console.log(error))
+                .catch(error => console.log(error));
+
+            function findDates() {
+                for (var i = 0; i < this.forecastArr.length; i++) {
+                    this.forecastDates = forecastArr[i].dt;
+                }
+            }
 
         },
         getCity() {
-            this.callAllFuncs()
+            this.callAllFuncs();
+//            this.getDates();
         }
     }
 })
